1c8c75fafcb6f6f4d03c475e0651435c053debe3
Talha O <talha43gs@gmail.com>
2023-11-07 18:54:10 +0000

    Test1 updated Login Func uploaded

---
+++ src/test/java/LoginFuncTest.java
@@ -0,0 +1,10 @@
+public class LoginFuncTest {
+
+    public static void main(String[] args) {
+        System.out.println("navigate to login page");
+        System.out.println("enter username");
+        System.out.println("enter password");
+        System.out.println("click login");
+        System.out.println("verify login page!!");
+    }
+}
