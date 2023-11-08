5fea3a516887916c33066cf7b9f5fa7ee11ff6f6
Talha O <talha43gs@gmail.com>
2023-11-08 18:26:27 +0000

    Test1 updated

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,6 +1,11 @@
 public class Test1 {
+
+    public static void testTalha(){
+        System.out.println("TALHA TEST PASSED");
+    }
     public static void main(String[] args) {
         System.out.println("test updated in talha branch !!!");
+        testTalha();
         System.out.println("Additional Tests!!!");
     }
 }
