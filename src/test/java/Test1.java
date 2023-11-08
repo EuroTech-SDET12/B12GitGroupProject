5bf69a9398ba8c65814129dd83ea821cfec00ae8
Talha O <talha43gs@gmail.com>
2023-11-08 18:20:12 +0000

    Alex test uploaded

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,6 +1,12 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("test1");
+        System.out.println("test1111111111111111111111111111111111111111");
+        testAlex();
         System.out.println("Login url added to common methods and params!!!");
+    }
+
+
+    public static void testAlex(){
+        System.out.println("TEST");
     }
 }
