35ce7e50b9df1de081542179a9aaf1ca663887ba
Pooja <gujalwarpooja@gmail.com>
2023-11-08 22:29:55 +0100

    New common test added

--- src/test/java/Group2/CommonTest_Group2.java
+++ src/test/java/Group2/CommonTest_Group2.java
@@ -3,10 +3,10 @@ package Group2;
 public class CommonTest_Group2 {
 
     public static void main(String[] args) {
-        System.out.println("Test 1");
-        System.out.println("Test 2");
+        System.out.println("Git 1");
+        System.out.println(" Git 2");
         System.out.println("Test 3");
-        System.out.println("Test 4");
-        System.out.println("Test 5");
+        System.out.println("This is git 4");
+        System.out.println("Git 4 5");
     }
 }
