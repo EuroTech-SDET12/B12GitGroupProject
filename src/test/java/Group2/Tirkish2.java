311a0040eb7d9dd44046d21f86e8edd5f65d9249
Tirkish M <tirkish1989@gmail.com>
2023-11-08 21:34:36 +0100

    Tirkish2 Updated

---
+++ src/test/java/Group2/Tirkish2.java
@@ -0,0 +1,10 @@
+package Group2;
+
+public class Tirkish2 {
+    public static void main(String[] args) {
+
+        System.out.println(" hello guys ");
+        System.out.println(" test 1");
+        System.out.println(" test2");
+    }
+}
