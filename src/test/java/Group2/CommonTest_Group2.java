22dc94b916cbfa38d16733f61327ea3d28df3b0f
Erkan <youremail@yourdomain.com>
2023-11-08 20:07:00 +0000

    Commit and push completed successfully

--- src/test/java/Group2/CommonTest_Group2.java
+++ src/test/java/Group2/CommonTest_Group2.java
@@ -3,10 +3,8 @@ package Group2;
 public class CommonTest_Group2 {
 
     public static void main(String[] args) {
-        System.out.println("Test 1");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
-        System.out.println("Test 4");
-        System.out.println("Test 5");
+        System.out.println("Erkan 1");
+        System.out.println("Hello World");
+        System.out.println("GitHub project");
     }
 }
