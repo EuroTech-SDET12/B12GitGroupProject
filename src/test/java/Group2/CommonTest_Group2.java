d34f1ff6b965f4de0a53dff1094a0df42b8f5056
Tirkish M <tirkish1989@gmail.com>
2023-11-08 21:06:19 +0100

    Tirkish Update

--- src/test/java/Group2/CommonTest_Group2.java
+++ src/test/java/Group2/CommonTest_Group2.java
@@ -4,7 +4,7 @@ public class CommonTest_Group2 {
 
     public static void main(String[] args) {
         System.out.println("Test 1");
-        System.out.println("Test 2");
+        System.out.println("Test 22222");
         System.out.println("Test 3");
         System.out.println("Test 4");
         System.out.println("Test 5 updated by Tirkish");
