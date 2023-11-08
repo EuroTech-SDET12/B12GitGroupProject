0c7470d3de7c6f8a058131cda29839d22ed8f214
Gylych1110 <149843995+Gylych1110@users.noreply.github.com>
2023-11-09 00:00:15 +0100

    ADD IT

---
+++ src/test/java/G3.java
@@ -0,0 +1,7 @@
+public class G3 {
+    public static void main(String[] args) {
+        System.out.println("G3 printed");
+        int a = 20;
+        System.out.println("What is happen");
+    }
+}
